df702abb8890463be99aecd52d1e54fdeb6cf302
lafayette96 <adek28000@gmail.com>
2024-10-01 14:18:04 +0200

    changed returned message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "KubeCon2023"
+    return "Hello from OpenShift"
  
 
 @app.route("/healthz")
